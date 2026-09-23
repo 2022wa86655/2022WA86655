7a49b294b8ad20f65edbe6f243827618de26fae3
2022wa86655 <2022wa86655@wilp.bits-pilani.ac.in>
2026-09-24 00:35:25 +0530

    Modify application message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven Jenkins CI Lab");
+        System.out.println("Hello World! -Jenkins Continuous Intergration");
     }
 }
